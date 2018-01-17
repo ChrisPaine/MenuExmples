c427d5e4ba414febc29e556102822b707c5c7a20
Chris Paine <cdpaine2003@gmail.com>
2018-01-16 21:39:29 -0500

    Updated

--- app/src/main/java/com/example/cpaine/menuexmples/MainActivity.java
+++ app/src/main/java/com/example/cpaine/menuexmples/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.cpaine.menuexmples;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_setting) {
+
+            DialogFragment myFragment = new MyDialogFragment();
+            myFragment.show(getFragmentManager(), "theDialog");
+
+            return true;
+        }else if(id == R.id.exit_the_app){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
